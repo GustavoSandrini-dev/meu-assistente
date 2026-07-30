04150b0590f22952826775a75f57322ccb477c3b
Gustavo Sandrini <sandrinigustavo@gmail.com>
2026-07-29 22:39:13 -0300

    Livros: datas compactas que cabem no card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "ma-v18";
+const C = "ma-v19";
 const SHELL = ["./","./index.html","./painel-financeiro-web.html","./biblioteca-jogos-web.html","./filmes-web.html","./livros-web.html","./common.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== C).map(x => caches.delete(x)))).then(() => self.clients.claim())); });
